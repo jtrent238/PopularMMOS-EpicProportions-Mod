6f86d8e2a10e7fcf089c4166485782b388be585b
Trent Patterson <jtrent238@outlook.com>
2017-05-08 21:11:21 -0400

    Add files via upload

---
+++ MineTest/epicproportions/init.lua
@@ -0,0 +1,50 @@
+-- default (Minetest 0.4 mod)
+-- Most default stuff
+
+-- The API documentation in here was moved into doc/lua_api.txt
+
+WATER_ALPHA = 160
+WATER_VISC = 1
+LAVA_VISC = 7
+LIGHT_MAX = 14
+
+-- Definitions made by this mod that other mods can use too
+default = {}
+
+-- Load other files
+dofile(minetest.get_modpath("epicproportions").."/mapgen.lua")
+
+-- Set a noticeable inventory formspec for players
+minetest.register_on_joinplayer(function(player)
+	local cb = function(player)
+		minetest.chat_send_player(player:get_player_name(), "This is the [minimal] \"Minimal Development Test\" game. Use [minetest_game] for the real thing.")
+	end
+	minetest.after(2.0, cb, player)
+end)
+
+--
+-- Tool definition
+--
+
+
+minetest.register_tool("epicproportions:pick_pat", {
+	description = "FuriousDestryoer Pickaxe",
+	inventory_image = "itemPatPick.png",
+	tool_capabilities = {
+		max_drop_level=0,
+		groupcaps={
+			cracky={times={[2]=2.00, [3]=1.20}, uses=10, maxlevel=1}
+		}
+	},
+})
+
+minetest.register_tool("epicproportions:jen_pat", {
+	description = "SuperGirlyGamer Pickaxe",
+	inventory_image = "itemJenPick.png",
+	tool_capabilities = {
+		max_drop_level=0,
+		groupcaps={
+			cracky={times={[2]=2.00, [3]=1.20}, uses=10, maxlevel=1}
+		}
+	},
+})
